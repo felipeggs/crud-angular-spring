59f76d9e013ce2153500611709d0b30e7372b78e
felipeggs <felipesidy9@gmail.com>
2025-03-14 00:28:56 -0300

    criando botoes

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -4,6 +4,7 @@ import { CoursesService } from '../services/courses.service';
 import { catchError, Observable, of } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
@@ -12,12 +13,14 @@ import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/err
 })
 export class CoursesComponent implements OnInit{
   courses$: Observable<Course[]>;
-  displayedColumns = ['name', 'category'];
+  displayedColumns = ['name', 'category', 'actions'];
 
 
   constructor(
     private coursesService: CoursesService,
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute
   ){
     this.courses$ = this.coursesService.list()
     .pipe(
@@ -36,6 +39,11 @@ export class CoursesComponent implements OnInit{
 
   ngOnInit(): void {
 
+  }
+
+  onAdd() {
+    debugger;
+    this.router.navigate(['courses/new'], {relativeTo: this.route});
   }
 
 }
